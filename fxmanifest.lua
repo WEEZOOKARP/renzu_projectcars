4c0ae4d0df1422fb6d315481dc85abc5900b5b61
WEEZOOKARP <76672483+WEEZOOKARP@users.noreply.github.com>
2022-01-18 01:45:36 +1300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,31 +4,30 @@ ui_page 'html/index.html'
 lua54 'on'
 
 shared_scripts {
-	"config.lua"
+'config.lua'
 }
 
 server_scripts {
-  '@mysql-async/lib/MySQL.lua',	
-  'config.lua',
-  'config_vehicles.lua',
-  'locale/*.lua',
-  'framework/sv_wrapper.lua',
-	"server.lua"
+'@oxmysql/lib/MySQL.lua',
+'config_vehicles.lua',
+'locale/.lua',
+'framework/sv_wrapper.lua',
+'server.lua'
 }
 client_scripts {
-	'config.lua',
-	'config_vehicles.lua',
-	'locale/*.lua',
-	'framework/cl_wrapper.lua',
-	"client.lua",
+'config.lua',
+'config_vehicles.lua',
+'locale/.lua',
+'framework/cl_wrapper.lua',
+'client.lua'
 }
 
 files {
-	'html/index.html',
-	'html/fonts/*',
-	'html/brands/*.png',
-	'html/parts/*.png',
-	'html/style.css',
-	'html/script.js',
-	'html/audio/*.ogg',
+'html/index.html',
+'html/fonts/',
+'html/brands/.png',
+'html/parts/.png',
+'html/style.css',
+'html/script.js',
+'html/audio/.ogg'
 }
